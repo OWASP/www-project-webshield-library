476fd01a9f62ad2909c56111d0826bf0c4b5bc97
Sreejith <hisreenair@gmail.com>
2026-06-23 23:25:54 -0500

    Delete backup files

--- backup/src/adapters/react/A01_broken_access_control/index.ts
+++
@@ -1,9 +0,0 @@
-// Context exports
-export * from "./context/ACLContext";
-
-// Hooks exports
-export * from "./hooks/useACL";
-
-// Component exports
-export * from "./components/WithACL";
-export * from "./components/ProtectedRoute";
--- backup/src/core/a01-broken-access-control/core/acl/index.ts
+++
@@ -1,5 +0,0 @@
-import { ACLPermission, ACLRole, ACLConfig } from "./interfaces";
-import { ACLManager } from "./acl-manager";
-
-export { ACLPermission, ACLRole, ACLConfig } from "./interfaces";
-export { ACLManager } from "./acl-manager";
